9d4961ba08e443648e63f640b8e9cfa8766ec4e4
worshix <worshiplivingstone@gmail.com>
2024-08-11 01:49:35 -0700

    finished training page

--- src/app/training/page.jsx
+++ src/app/training/page.jsx
@@ -1,18 +1,21 @@
 import TrainingCard from "@/components/TrainingCard";
 import Services from "../../../public/assets/Services";
+import dynamic from "next/dynamic";
+
+const BasicAnimations = dynamic(() => import('./../../components/BasicAnimations'), { ssr: false });
 
 function Training(){
     return(
-        <>
+        <main>
         <section>
-            <h1 className="text-orange-400 text-center text-xl font-bold mt-4 mb-2 my-4">Training</h1>
-            <article className="flex justify-center flex-col gap-2 sm:flex-row flex-wrap mt-4">
+        <h1 key='automation-header' className="text-orange-400 text-center text-xl font-bold my-4 md:text-2xl md:my-6 slide-in-top">Training</h1>
+        <article className="flex justify-center flex-col gap-2 sm:flex-row flex-wrap mt-4">
                 {Services.training.map((item) => <TrainingCard prop = {item}/>)}
             </article>
-            <article className=" mt-2 flex gap-2 flex-wrap">
+            <article className=" mt-6 flex gap-2 flex-wrap justify-center space-y-6 items-top">
                 <div>
-                    <h1 className="font-bold text-center">Training Methodology</h1>
-                    <ul className="list-disc pl-6">
+                    <h1 className="text-orange-400 font-bold text-center slide-in-left">Training Methodology</h1>
+                    <ul className="list-disc pl-6 slide-in-top">
                         <li>Theoretical lectures</li>
                         <li>Practical hands-on sessions</li>
                         <li>Project-based learning</li>
@@ -21,8 +24,8 @@ function Training(){
                     </ul>
                 </div>
                 <div>
-                    <h1 className="font-bold text-center">Training Materials</h1>
-                    <ul className="list-disc pl-6">
+                    <h1 className="text-orange-400 font-bold text-center slide-in-left">Training Materials</h1>
+                    <ul className="list-disc pl-6 slide-in-top">
                         <li>Comprehensive training manual</li>
                         <li>Software and tools for programming and design</li>
                         <li>Hardware kits for hands-on practice</li>
@@ -30,8 +33,8 @@ function Training(){
                     </ul>
                 </div>
                 <div>
-                    <h1 className="font-bold text-center">Prerequisites</h1>
-                    <ul className="list-disc pl-6">
+                    <h1 className="text-orange-400 font-bold text-center slide-in-left">Prerequisites</h1>
+                    <ul className="list-disc pl-6 slide-in-top">
                         <li>Basic knowledge of electronics and programming</li>
                         <li>Familiarity with circuit analysis and design</li>
                         <li>No prior experience with solar installations or PLC programming required</li>
@@ -39,7 +42,19 @@ function Training(){
                 </div>
             </article>
         </section>
-        </>
+            {/* all posts to do with training services */}
+            <h1 className='text-center font-bold text-2xl sm:text-3xl text-main-400 m-4 slide-in-top'>Activities</h1>
+            <section className="training-services">
+
+            </section>
+
+            {/* testimonials for training services */}
+            <h1 className='text-center font-bold text-2xl sm:text-3xl text-main-400 m-4 slide-in-top'>Testimonials</h1>
+            <section className="training-testimonials">
+
+            </section>
+        <BasicAnimations />
+        </main>
     )
 }
 
