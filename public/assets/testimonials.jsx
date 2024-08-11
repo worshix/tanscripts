c23c294615171b03a2a9fc7a32ab807e912ab9ca
worshix <worshiplivingstone@gmail.com>
2024-08-11 11:50:15 -0700

    added fake testimonials object

--- public/assets/testimonials.jsx
+++ public/assets/testimonials.jsx
@@ -1,6 +1,37 @@
 const testimonials = [
     {
-        
+        image:'/assets/client-image.jpeg',
+        name:'Worshishi the Great',
+        company:'pixelCrafte',
+        jobTitle:'FrontEnd Developer',
+        contactDetails:'Mondibata pamhepo chete',
+        links:[
+            {
+                label:'phone',
+                href:'tel:+263719610669'
+            },
+            {
+                label:'email',
+                href:'mailto:pixelcraftewt@gmail.com'
+            },
+        ]
+    },
+    {
+        image:'/assets/client-image.jpeg',
+        name:'Worshan\'a',
+        company:'Meddes',
+        jobTitle:'Communications Engineer',
+        contactDetails:'Mondibata ku office',
+        links:[
+            {
+                label:'phone',
+                href:'tel:+263719610669'
+            },
+            {
+                label:'email',
+                href:'mailto:pixelcraftewt@gmail.com'
+            },
+        ]
     },
 ]
 
