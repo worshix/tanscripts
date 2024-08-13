d5871cf48861576d798390b1beeeef6dedc51888
worshix <worshiplivingstone@gmail.com>
2024-08-13 00:07:34 -0700

    videos appearing way

--- src/components/VideoGallery.jsx
+++ src/components/VideoGallery.jsx
@@ -3,10 +3,11 @@ function VideoGallery({ videos }) {
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
             {videos.map((video, index) => (
                 <div key={index} className="flex flex-col items-center bg-white rounded-lg overflow-hidden shadow-md">
-                    <div className="w-full h-64 relative">
-		    	<div className="h-full w-auto mx-auto"
-  				dangerouslySetInnerHTML={{ __html: video.data.video.html }}
-			/>
+                    <div className="w-full h-0 relative pb-[56.25%] video-container">
+                        <div
+                            dangerouslySetInnerHTML={{ __html: video.data.video.html }}
+                            className="absolute top-0 left-0 w-full h-full"
+                        />
                     </div>
                     <p className="p-4 text-center text-main-300 font-semibold">{video.data.description}</p>
                 </div>
@@ -16,7 +17,3 @@ function VideoGallery({ videos }) {
 }
 
 export default VideoGallery;
-/**<video controls className="h-full w-auto mx-auto">
-                            <source src={video.src} type={video.type} />
-                            Your browser does not support the video tag.
-                        </video>*/
