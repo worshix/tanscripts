0ce3b9374812ddad1a63384af7ec40e8df77d94f
worshix <153189473+worshix@users.noreply.github.com>
2024-09-11 01:24:05 +0200

    Update TrainingCard.jsx

--- src/components/TrainingCard.jsx
+++ src/components/TrainingCard.jsx
@@ -9,7 +9,7 @@ export default function TrainingCard(prop){
             <div className="w-[300px] h-[100px] bg-main-300"></div>
             <div className="p-1 bg-main-300 w-fit rounded-full mx-auto relative -mt-[50px]"><div className="bg-white h-[90px] w-[90px] rounded-full flex justify-center items-center"><i className="block bi bi-mortarboard text-slate-500 text-5xl"></i></div></div>
             <h1 className="font-bold text-center">{title}</h1>
-            <Link className="block w-fit p-1 my-4 font-bold text-sm m-2 mb-auto underline underline-offset-1 text-orange-400" href='contactus'>Begin Training</Link>
+            <Link className="block w-fit p-1 my-4 font-bold text-sm m-2 mb-auto underline underline-offset-1 text-orange-400" href='/contactus'>Begin Training</Link>
             <ul className="pl-6 p-2 list-disc mb-auto">
                 {details.map((item, index) => (<li key={"detail-item"+index}>{item}</li>))}
             </ul>
