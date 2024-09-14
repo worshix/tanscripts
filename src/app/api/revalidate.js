4dd3a9a9f4ea9bdb6c75dd3912dd2d9264d4dc0b
root <root@DESKTOP-BUGKGO7>
2024-09-15 00:52:25 +0200

    webhook revalidate api

--- src/app/api/revalidate.js
+++ src/app/api/revalidate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function POST(req, res) {
   // Ensure this is a POST request from Prismic Webhook
   if (req.method === 'POST') {
     try {
