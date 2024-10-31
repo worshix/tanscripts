f38f67d16b55ad446715b082dba0809ff7e41954
worshix <153189473+worshix@users.noreply.github.com>
2024-10-31 09:58:16 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 
 export const metadata = { 
   description:
-    "Tanscripts Engineering specializes in providing cutting-edge industrial automation, control systems, embedded systems and engineering solutions. We help manufacturers optimize operations, increase efficiency, and reduce downtime through advanced automation technologies. We also provide Indtrial automation, solar systems and embedded systems training",
+    "Zimtech Engineering specializes in providing cutting-edge industrial automation, control systems, embedded systems and engineering solutions. We help manufacturers optimize operations, increase efficiency, and reduce downtime through advanced automation technologies. We also provide Indtrial automation, solar systems and embedded systems training",
   keywords: [
     "industrial automation",
     "control systems",
@@ -16,22 +16,22 @@ export const metadata = {
     "process optimization",
     "SCADA systems",
     "PLC training",
-    "Tanscripts Engineering",
+    "Zimtech Engineering",
     "embedded systems",
   ],
   icons:{
     icon:'/assets/1.png'
   },
-  author: "Tanscripts Engineering",
+  author: "Zimtech Engineering",
   og: {
-    title: "Tanscripts Engineering | Industrial Automation & Engineering Solutions",
+    title: "Zimtech Engineering | Industrial Automation & Engineering Solutions",
     description:
-      "At Tanscripts Engineering, we offer top-tier industrial automation and engineering services to enhance manufacturing processes, improve efficiency, and deliver state-of-the-art control systems. Explore our expertise in automation.",
+      "At Zimtech Engineering, we offer top-tier industrial automation and engineering services to enhance manufacturing processes, improve efficiency, and deliver state-of-the-art control systems. Explore our expertise in automation.",
     type: "website",
-    url: "https://www.tanscriptsengineering.co.zw",  // Replace with actual URL
+    url: "https://www.zimtechengineering.co.zw",  // Replace with actual URL
     image: "/assets/1.png", // Ensure this image exists
   },
-  canonical: "https://www.tanscriptsengineering.co.zw",  // Replace with actual URL
+  canonical: "https://www.zimtechengineering.co.zw",  // Replace with actual URL
   robots: "index, follow", // Helps search engines index your content
 };
 export default function RootLayout({ children }) {
