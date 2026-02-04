15632b20c2abd08c19a6f5de1c24e48c90a909c5
worshix <worshiplivingstone@gmail.com>
2026-02-04 18:09:14 +0200

    deleted all

--- public/socials.jsx
+++
@@ -1,9 +0,0 @@
-const socials = {
-    facebookServices : 'https://www.facebook.com/share/14Xv342dM5/',
-    faceBookTraining : 'https://www.facebook.com/share/1H9xREjaFk/', 
-    phone: '263713664345',
-    info: 'info@zimtechengineering.co.zw',
-    training: 'training@zimtechengineering.co.zw'
-}
-
-export default socials;
